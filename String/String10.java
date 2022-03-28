2ea985bafa5a584dc304193e6647ef17beed8d6e
jiwon <jjiwon0768@gmail.com>
2022-03-28 14:21:46 +0900

    220328

--- String/String10.java
+++ String/String10.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class String10 {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-
         int n = sc.nextInt();
 
         int count = 0;
