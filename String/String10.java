b5bd2c45ec95284ffb48827c73e420f5c990d904
jiwon <jjiwon0768@gmail.com>
2022-03-28 14:19:12 +0900

    220328

--- String/String10.java
+++ String/String10.java
@@ -3,37 +3,39 @@ package String;
 import java.util.Scanner;
 
 public class String10 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        String s = sc.next();
+
         int count = 0;
-        boolean[] check = new boolean[26];
-        int before = 0;
 
-        for(int j = 0; j < n; j++) {
-            for(int i = 0; i < s.length(); i++) {
-                int now = s.charAt(i);
-
-                if(before != now) {
-
-                    if(!check[now - 'a']) {
-                        check[now - 'a'] = true;
-                        before = now;
-                    }
-                    else {
-                        break;
-                    }
-                }
+        for(int i = 0; i < n; i++) {
+            if(check() == true) {
+                count++;
             }
-
-            for(int k = 0; k < s.length(); k++) {
-                if(check[k] == true) {
-                    count++;
-                }
-            }
-
         }
         System.out.println(count);
+    }
+
+    public static boolean check() {
+        int prev = 0;
+        boolean[] check = new boolean[26];
+
+        String s = sc.next();
+        for (int j = 0; j < s.length(); j++) {
+            int now = s.charAt(j);
+
+            if (prev != now) {
+                if (!check[now - 'a']) {
+                    check[now - 'a'] = true;
+                    prev = now;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
