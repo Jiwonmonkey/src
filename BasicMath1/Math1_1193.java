2f0b1a948eabb9bb67853fd0cc4c382d7dc945ec
jiwon <jjiwon0768@gmail.com>
2022-03-30 13:20:49 +0900

    220330 수정

--- BasicMath1/Math1_1193.java
+++ BasicMath1/Math1_1193.java
@@ -12,16 +12,20 @@ public class Math1_1193 {
         int top;
         int btm;
 
+        //몇번째 라인에 속하는지 찾는 코드
         while(x > sum) {
             line++;
             sum += line;
         }
 
+        //그 라인의 첫번째 자리(index)를 찾는 코드
         for(int i = 1; i < line; i++) {
             start = start + i;
         }
 
+        //line이 1보다 클 때
         if(line > 1) {
+            //짝수는 분모가 라인으로 분자가 1로 시작
             if(line % 2 == 0) {
                 top = 1;
                 btm = line;
@@ -33,6 +37,7 @@ public class Math1_1193 {
                 }
                 System.out.println(top + "/" + btm);
             }
+            //홀수는 그 반대로 시작함
             else {
                 top = line;
                 btm = 1;
@@ -44,6 +49,7 @@ public class Math1_1193 {
                 System.out.println(top + "/" + btm);
             }
         }
+        // 1일 때 항상 1 / 1 임
         else {
             System.out.println(1 + "/" + 1);
         }
