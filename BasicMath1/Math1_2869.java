a02db17380969c742b208f7d98acc792f6c2605c
jiwon <jjiwon0768@gmail.com>
2022-03-31 14:36:39 +0900

    220330 수정

--- BasicMath1/Math1_2869.java
+++ BasicMath1/Math1_2869.java
@@ -1,7 +1,25 @@
 package BasicMath1;
 
-public class Math1_2869 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class Math1_2869 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int move = a - b; // 하루 이동 거리
+        int days = v / move;
+
+        if(v % move != 0) {
+            days++;
+        }
+
+        System.out.println(days);
     }
 }
