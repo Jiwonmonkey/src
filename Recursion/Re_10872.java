175f7986c833a39d997e6787250cbeb8c2ae100d
jiwon <jjiwon0768@gmail.com>
2022-04-25 18:26:44 +0900

    220423

--- Recursion/Re_10872.java
+++ Recursion/Re_10872.java
@@ -1,4 +1,27 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Re_10872 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int fac = Factorial(n);
+
+        System.out.println(fac);
+
+    }
+
+    public static int Factorial(int n){
+        if(n == 0) {
+            return 1;
+        }
+        else {
+            return n * Factorial(n-1);
+        }
+
+    }
 }
