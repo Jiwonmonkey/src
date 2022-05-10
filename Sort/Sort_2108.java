68dcd42b00a0fef2d5595432e9728c8d33d770fe
jiwon <jjiwon0768@gmail.com>
2022-05-10 11:03:56 +0900

    220510 통계학 (산술 평균, 중앙값, 최빈값)

--- Sort/Sort_2108.java
+++ Sort/Sort_2108.java
@@ -1,57 +1,59 @@
 package Sort;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Sort_2108 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[8001];
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int median = 10000;
+        int mode = 10000;
 
         for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            int value = sc.nextInt();
+            sum += value;
+            arr[i + 4000]++;
+
+            if(max < value) {
+                max = value;
+            }
+            if(min > value) {
+                min = value;
+            }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        int count = 0; // 중앙값 빈도 누적수
+        int mode_max = 0; // 최빈값 최댓값
+        boolean flag = false; // 최빈값이 이전에 한번만 출연했을 경우 true, 아닐 경우 false
+        for(int i = min + 4000; i <= max + 4000; i++) {
+            if(arr[i] > 0) {
+
+                // 중앙값 찾기
+                if(count < (n + 1) / 2) {
+                    count += arr[i];
+                    median = i - 4000;
+                }
+
+                // 최빈값 찾기
+                if(mode_max < arr[i]) {
+                    mode_max = arr[i];
+                    flag = true;
+                }
+                else if(mode_max == arr[i] && flag) {
+                    mode_max = arr[i];
+                    flag = false;
                 }
             }
         }
 
-        System.out.print("정렬 arr : ");
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
-
-        System.out.println();
-        // 산술 평균
-        int sum = 0;
-        for(int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        System.out.println("1. 산술 평균 : " + (sum / n));
-
-
-        // 중앙값
-        int medium = n / 2;
-        System.out.println("2. 중앙값 : " + arr[medium]);
-
-
-        // 최빈값
-        boolean[] count = new boolean[10000];
-        int num = 1;
-        for(int i = 0; i < arr.length; i++) {
-            if(!count[i]) {
-
-            }
-        }
-
+        System.out.println((int)Math.round((double)sum / n));
+        System.out.println(median);
+        System.out.println(mode);
+        System.out.println(max - min);
     }
 }
