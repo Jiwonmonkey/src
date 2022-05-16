5983f4d0f875abec3d0d4e1f6ff27969e4ca1078
jiwon <jjiwon0768@gmail.com>
2022-05-16 17:20:29 +0900

    220516 comparator을 이용한 arrays.sort 람다식을 통해 문제 해결

--- Sort/Sort_11651.java
+++ Sort/Sort_11651.java
@@ -3,9 +3,8 @@ package Sort;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Sort_11650 {
+public class Sort_11651 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] arr = new int[n][2];
@@ -16,14 +15,15 @@ public class Sort_11650 {
         }
 
         Arrays.sort(arr, (e1, e2) -> {
-            if(e1[0] == e2[0]) {
-                return e1[1] - e2[1];
-            }
-            else {
-                return e1[0] - e2[0];
-            }
+           if(e1[1] == e2[1]) {
+               return e1[0] - e2[0];
+           }
+           else {
+               return e1[1] - e2[1];
+           }
         });
-        for(int i = 0; i < n; i++) {
+
+        for(int i = 0; i < n ; i++) {
             System.out.print(arr[i][0] + " ");
             System.out.println(arr[i][1]);
         }
